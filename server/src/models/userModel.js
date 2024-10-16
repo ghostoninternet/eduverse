6d98941a23a8e2638425df1967bb8ee9f72e56ab
phucpham <phamhuuphuc4049@gmail.com>
2024-10-16 19:51:34 +0700

    "completed signup API"

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true}
 });
 
 const User = mongoose.model("User", userSchema);
