b832abf30d5d70594d35cb10e629a068ce7b2b27
phucpham <phamhuuphuc4049@gmail.com>
2024-10-16 19:51:34 +0700

    "completed signup API"

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import User from "./src/models/userModel.js"; //use for testing connection
 import "dotenv/config";
-import registerRoute from './src/routes/Register/registerRoute.js'
-import loginRoute from './src/routes/Login/loginRoute.js'
+import registerRoute from "./src/routes/auth/registerRoute.js";
+import loginRoute from "./src/routes/auth/loginRoute.js";
+import secretRoute from "./src/routes/secretRoute.js";
 const app = express();
 
 //connect db
@@ -18,13 +20,12 @@ try {
 // const result = await User.find({});
 // console.log(result);
 
-app.get("/", (req, res, next) => {
-  res.status(200).send("Hello World!");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', registerRoute);
-app.use('/', loginRoute);
-
+app.use("/", registerRoute);
+app.use("/", loginRoute);
+app.use("/", secretRoute);
 app.listen(8000, () => {
   console.log("Server start on http://localhost:8000");
 });
