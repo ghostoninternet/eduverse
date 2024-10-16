95632e6a9d39ef1cdddcdbac3f53c163251be127
phucpham <phamhuuphuc4049@gmail.com>
2024-10-16 21:28:38 +0700

    save new user with hash password

--- server/src/controllers/auth/registerController.js
+++ server/src/controllers/auth/registerController.js
@@ -1,28 +1,34 @@
 import User from "../../models/userModel.js";
-
+import bcrypt from 'bcrypt'
 export const Register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    
+    const existedUser = await User.findOne({email}) || await User.findOne({username})
+
+    if(existedUser){
+      return res.status(501).json({message: "User existed or you must fill all the fields"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
     const newUser = new User({
       username: username,
       email: email,
-      password: password,
-    });
-    const existedUser =
-      (await User.findOne({ email: email })) ||
-      (await User.findOne({ username: username }));
-
-    if (existedUser) {
-      //neu da ton tai nguoi dung hoac chua nhap du thong tin
-      return res.status(501).json({ success: false, error: "Check input again!" });
-    }
-
+      password: hashedPassword
+    })
+    
     await newUser.save();
 
     res.status(201).json({
       success: true,
-      newUser,
-    });
+      data:{
+        userId: newUser._id,
+        email: newUser.email,
+        password: hashedPassword
+      }
+    })
+    
   } catch (error) {
     res.status(500).json({ error:"error"});
   }
