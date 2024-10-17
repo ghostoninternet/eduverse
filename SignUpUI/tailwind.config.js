bbe49343365d05b95ea6e2a7a6af35c1ca5828f9
quanpva62 <phamquan6023@gmail.com>
2024-10-17 22:51:37 +0700

    Signup demo

---
+++ SignUpUI/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}',  // Đảm bảo đúng đường dẫn các file React
+    './build/*.html'   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
