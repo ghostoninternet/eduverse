1e2fcc9b470dc01196afcf1cc182ef4ac6712148
ghostoninternet <daosyphuc288@gmail.com>
2024-10-19 22:10:39 +0700

    feature: Database model

---
+++ server/src/models/courseModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+  courseTitle: {
+    type: String,
+    required: true,
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+  },
+  courseCategory: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: 'Categories',
+  },
+  coursePrice: {
+    type: Number,
+    required: true,
+  },
+  courseModules: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: 'Modules'
+  },
+  courseReviews: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+    ref: 'Reviews'
+  },
+  courseReviewCount: {
+    type: Number,
+    default: 0,
+  },
+  courseRatingAvg: {
+    type: Number,
+    default: 0,
+  }
+}, {
+  timestamps: true
+});
+
+const Courses = mongoose.model("Courses", courseSchema);
+
+export default Courses;
