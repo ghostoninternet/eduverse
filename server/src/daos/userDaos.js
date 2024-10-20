abc26e95a9b60d1d04202393fecf3263d33595a6
ghostoninternet <daosyphuc288@gmail.com>
2024-10-20 22:29:45 +0700

    feature(BE): Sign In and Logout

---
+++ server/src/daos/userDaos.js
@@ -0,0 +1,38 @@
+import CustomError from '../errors/customError.js'
+import userModel from "../models/userModel"
+import getSelectData from "../utils/getSelectData"
+import getUnselectData from "../utils/getUnselectData"
+
+const findOneUser = async (filter) => {
+  return await userModel.findOne(filter).select(getUnselectData(['password, __v']))
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError("Something went wrong in findOneUser")
+    })
+}
+
+const findManyUsers = async (filter, select) => {
+  return await userModel.find(filter).select(getSelectData(select))
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError("Something went wrong in findManyUsers")
+    })
+}
+
+const createNewUser = async (userDocument) => {
+  return await userModel.create(userDocument)
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError("Something went wrong in createNewUser")
+    })
+}
+
+
+export default {
+  findOneUser,
+  findManyUsers,
+  createNewUser,
+}
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import authControllers from '../controllers/authControllers.js'
 import asyncHandler from '../middlewares/asyncHandler.js'
+import authToken from '../middlewares/authToken.js'
 
 const authRouter = express.Router()
 
 authRouter.post('/register', asyncHandler(authControllers.register))
+authRouter.post('/login', asyncHandler(authControllers.login))
+router.post('/refresh-token', asyncHandler(authControllers.refreshToken))
+
+authRouter.use(authToken)
+authRouter.post('/logout', asyncHandler(authControllers.logout))
 
 export default authRouter
