b4055e8196c845373fe3d568c0ca960dba0ef1e4
phucpham <phamhuuphuc4049@gmail.com>
2024-11-06 00:05:48 +0700

    api get homepage course(unauthorized)

--- server/src/routes/course.js
+++ server/src/routes/course.js
@@ -4,5 +4,7 @@ import courseControllers from '../controllers/courseControllers.js'
 const courseRouter = express.Router()
 
 courseRouter.get('/search', asyncHandler(courseControllers.searchCourses))
-
+courseRouter.get('/recommended', asyncHandler(courseControllers.getRecommendedCourses))
+courseRouter.get('/free', asyncHandler(courseControllers.getFreeCourses))
+courseRouter.get('/popular', asyncHandler(courseControllers.getMostPopularCourses))
 export default courseRouter
