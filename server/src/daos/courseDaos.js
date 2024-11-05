b0747c613836d51aa877af740b13a1e5e996908c
phucpham <phamhuuphuc4049@gmail.com>
2024-11-06 00:05:48 +0700

    api get homepage course(unauthorized)

--- server/src/daos/courseDaos.js
+++ server/src/daos/courseDaos.js
@@ -1,6 +1,63 @@
 import Courses from "../models/courseModel.js"
 import CustomError from '../errors/customError.js'
 
+const findRecommendedCourses = async () => {
+  return await Courses.aggregate([
+    {
+      $sort: {"courseRatingAvg" : -1} //-1: sort descending
+    },
+    {
+      //get 3 fields: title, imgURL, description
+      $project: {
+        "courseTitle" : 1,
+        "courseImgUrl": 1,
+        "courseDescription": 1
+      }
+    },
+    {
+      $limit: 4
+    }
+  ])
+}
+
+const findFreeCourses = async () => {
+   return await Courses.aggregate([
+    {
+      $match: {coursePrice: 0}
+    },
+    {
+      $sort: {"courseRatingAvg": -1}
+    },
+    {
+      $project: {
+        "courseTitle" : 1,
+        "courseImgUrl": 1,
+        "courseDescription": 1
+      }
+    },
+    {
+      $limit: 4
+    } 
+   ])
+}
+
+const findMostPopularCourses = async () =>{
+    return await Courses.aggregate([
+      {
+        $sort: {"courseLearnerCount": -1} //sort by descending
+      },
+      {
+        $project: {
+          "courseTitle" : 1,
+          "courseImgUrl": 1,
+          "courseDescription": 1
+        }
+      },
+      {
+        $limit: 4
+      }
+    ])
+}
 const countNumberOfCourses = async (filter={}) => {
   return await Courses.countDocuments(filter)
   .then(data => data)
@@ -22,4 +79,7 @@ const findCourses = async (filter={}, limit=12, page=1) => {
 export default {
   countNumberOfCourses,
   findCourses,
+  findRecommendedCourses,
+  findFreeCourses,
+  findMostPopularCourses
 }
