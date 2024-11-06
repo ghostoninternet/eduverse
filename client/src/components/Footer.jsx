2bbae4cc58d588654b16390aeb317131c6079baf
phucpham <phamhuuphuc4049@gmail.com>
2024-11-06 10:45:58 +0700

    UI homepage(unauthorized)

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-16 flex items-center border-t-2 border-gray-500">
+      <div>
+        <p className="px-8 font-semibold">© 2024 Hust Academy Inc. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
