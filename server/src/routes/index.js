a8205e93256dac225498568e23498f7e55f2e9c0
ghostoninternet <daosyphuc288@gmail.com>
2024-11-10 16:44:49 +0700

    feature: API CRUD course review

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import authRouter from './auth.js'
 import courseRouter from './course.js'
+import reviewRouter from './review.js'
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
 router.use('/courses', courseRouter)
+router.use('/review', reviewRouter)
 
 export default router
