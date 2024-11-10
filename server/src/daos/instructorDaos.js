2cd2446a0c0c7b18f92f556a52de2172fc77c016
ghostoninternet <daosyphuc288@gmail.com>
2024-11-10 14:40:18 +0700

    feature: API get course detail

--- server/src/daos/instructorDaos.js
+++ server/src/daos/instructorDaos.js
@@ -1,3 +1,14 @@
+import Instructors from '../models/instructorModel'
+
 const findInstructorById = async (instructorId) => {
-  
+  return await Instructors.findById(instructorId)
+  .then(data => data)
+  .catch(err => {
+    console.log(err)
+    throw new CustomError.DatabaseError()
+  })
+}
+
+export default {
+  findInstructorById,
 }
