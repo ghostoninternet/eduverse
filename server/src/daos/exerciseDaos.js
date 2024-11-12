e0f1580f1457ec7a9bc8bbac30b052da57e75a1c
ghostoninternet <daosyphuc288@gmail.com>
2024-11-12 15:45:15 +0700

    feature: Instructor API exercise management

--- server/src/daos/exerciseDaos.js
+++ server/src/daos/exerciseDaos.js
@@ -1,14 +1,68 @@
 import Exercises from "../models/exerciseModel.js"
 
+const countExercises = async (filter) => {
+  return await Exercises.countDocuments(filter)
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
+}
+
 const findExerciseById = async (exerciseId) => {
   return await Exercises.findById(exerciseId)
-  .then(data => data)
-  .catch(err => {
-    console.log(err)
-    throw new CustomError.DatabaseError()
-  })
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
+}
+
+const findExercises = async (filter, limit = 10, page = 1) => {
+  return await Exercises.find(filter).skip((page - 1) * limit).limit(limit)
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
+}
+
+const createNewExercise = async (newExerciseDocument) => {
+  return await Exercises.create(newExerciseDocument)
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
+}
+
+const updateExercise = async (exerciseId, updateExerciseData) => {
+  return await Exercises.findOneAndUpdate(
+    { _id: exerciseId },
+    updateExerciseData,
+    { new: true }
+  )
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
+}
+
+const deleteExercise = async (exerciseId) => {
+  return await Exercises.findOneAndDelete({ _id: exerciseId })
+    .then(data => data)
+    .catch(err => {
+      console.log(err)
+      throw new CustomError.DatabaseError()
+    })
 }
 
 export default {
+  countExercises,
+  findExercises,
   findExerciseById,
+  createNewExercise,
+  updateExercise,
+  deleteExercise,
 }
