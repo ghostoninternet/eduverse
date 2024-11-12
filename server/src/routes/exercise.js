226f9e6f7ceca8c4e2fa1ebcfc83eda75db37a56
ghostoninternet <daosyphuc288@gmail.com>
2024-11-12 15:45:19 +0700

    feature: Instructor API exercise management

--- server/src/routes/exercise.js
+++ server/src/routes/exercise.js
@@ -5,10 +5,10 @@ import exerciseControllers from '../controllers/exerciseControllers.js'
 const exerciseRouter = express.Router()
 
 exerciseRouter.get('/instructor', asyncHandler(exerciseControllers.getExercises))
-exerciseRouter.get('/instructor/:moduleId', asyncHandler(exerciseControllers.getExerciseDetail))
+exerciseRouter.get('/instructor/:exerciseId', asyncHandler(exerciseControllers.getExerciseDetail))
 exerciseRouter.get('/instructor/search', asyncHandler(exerciseControllers.searchExercise))
 exerciseRouter.post('/instructor', asyncHandler(exerciseControllers.createNewExercise))
-exerciseRouter.put('/instructor/:moduleId', asyncHandler(exerciseControllers.updateExercise))
-exerciseRouter.delete('/instructor/:moduleId', asyncHandler(exerciseControllers.deleteExercise))
+exerciseRouter.put('/instructor/:exerciseId', asyncHandler(exerciseControllers.updateExercise))
+exerciseRouter.delete('/instructor/:exerciseId', asyncHandler(exerciseControllers.deleteExercise))
 
 export default exerciseRouter
