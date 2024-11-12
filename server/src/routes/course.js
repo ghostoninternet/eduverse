1d3e30a4d67830a91a047d38155f075a245b3fdc
ghostoninternet <daosyphuc288@gmail.com>
2024-11-12 23:34:48 +0700

    feature: instructor api course management

--- server/src/routes/course.js
+++ server/src/routes/course.js
@@ -8,4 +8,13 @@ courseRouter.get('/search', asyncHandler(courseControllers.searchCourses))
 courseRouter.get('/recommended', asyncHandler(courseControllers.getRecommendedCourses))
 courseRouter.get('/free', asyncHandler(courseControllers.getFreeCourses))
 courseRouter.get('/popular', asyncHandler(courseControllers.getMostPopularCourses))
+
+// For instructor, optimized later :v
+courseRouter.get('/instructor', asyncHandler(courseControllers.getInstructorCourses))
+courseRouter.get('/instructor/:courseId', asyncHandler(courseControllers.getInstructorCourseDetail))
+courseRouter.get('/instructor/search', asyncHandler(courseControllers.searchInstructorCourses))
+courseRouter.post('/instructor', asyncHandler(courseControllers.createNewCourse))
+courseRouter.put('/instructor/:exerciseId', asyncHandler(courseControllers.updateCourse))
+courseRouter.delete('/instructor/:exerciseId', asyncHandler(courseControllers.deleteCourse))
+
 export default courseRouter
