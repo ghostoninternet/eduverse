0a9c353b625780e806f98cb70b324be217630a75
phucpham <phamhuuphuc4049@gmail.com>
2024-11-17 13:02:11 +0700

    add sticky header

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Course = (props) => {
 
   return (
-    <div className="border rounded-md p-4 shadow-md hover:scale-105 flex flex-col">
+    <div className="border rounded-md p-4 shadow-md hover:scale-105 flex flex-col hover:-z-10">
       <img src={`${props.imgUrl}`} className="w-full h-full"/>
       <div className="mt-4">
         <h3 className="break-words font-bold">{props.title}</h3>
