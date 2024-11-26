c7f7e39e34a6cc8d0be4f5780b63939ef9dce8bd
phucpham <phamhuuphuc4049@gmail.com>
2024-11-26 21:52:45 +0700

    fix api Review, connect some to frontend

---
+++ client/src/apis/course/getCourseDetail.js
@@ -0,0 +1,14 @@
+import customFetch from "../../helpers/customFetch";
+
+const getCourseDetail = async (courseId) => {
+  const url = `http://localhost:8000/api/courses/${courseId}`;
+  try {
+    const response = await customFetch(url);
+    const json = await response.json();
+    return json
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getCourseDetail;
