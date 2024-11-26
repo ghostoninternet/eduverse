eb9ed2a868860d4c0cf793b0365b223f20ddeef0
phucpham <phamhuuphuc4049@gmail.com>
2024-11-27 01:31:48 +0700

    connect api user enrolled course list

--- server/src/daos/enrolledCourseDaos.js
+++ server/src/daos/enrolledCourseDaos.js
@@ -1,24 +1,29 @@
-import EnrolledCourses from "../models/enrolledCourseModel.js"
+import EnrolledCourses from "../models/enrolledCourseModel.js";
+import courseDaos from "./courseDaos.js";
 
 const findEnrolledCourse = async (filter) => {
   return await EnrolledCourses.findOne(filter)
-    .then(data => data)
-    .catch(err => {
-      console.log(err)
-      throw new CustomError.DatabaseError()
-    })
-}
+    .then((data) => data)
+    .catch((err) => {
+      console.log(err);
+      throw new CustomError.DatabaseError();
+    });
+};
 
 const createNewEnrolledCourse = async (newEnrolledCourseData) => {
   return await EnrolledCourses.create(newEnrolledCourseData)
-    .then(data => data)
-    .catch(err => {
-      console.log(err)
-      throw new CustomError.DatabaseError()
-    })
-}
+    .then((data) => data)
+    .catch((err) => {
+      console.log(err);
+      throw new CustomError.DatabaseError();
+    });
+};
 
-const updateEnrolledCourse = async (userId, courseId, updateEnrolledCourseDocument) => {
+const updateEnrolledCourse = async (
+  userId,
+  courseId,
+  updateEnrolledCourseDocument
+) => {
   return await EnrolledCourses.findOneAndUpdate(
     {
       userId: userId,
@@ -26,37 +31,58 @@ const updateEnrolledCourse = async (userId, courseId, updateEnrolledCourseDocume
     },
     updateEnrolledCourseDocument,
     {
-      new: true
+      new: true,
     }
   )
-    .then(data => data)
-    .catch(err => {
-      console.log(err)
-      throw new CustomError.DatabaseError()
-    })
-}
+    .then((data) => data)
+    .catch((err) => {
+      console.log(err);
+      throw new CustomError.DatabaseError();
+    });
+};
 
 const deleteEnrolledCourse = async (filter) => {
   return await EnrolledCourses.findOneAndDelete(filter)
-    .then(data => data)
-    .catch(err => {
-      console.log(err)
-      throw new CustomError.DatabaseError()
-    })
-}
+    .then((data) => data)
+    .catch((err) => {
+      console.log(err);
+      throw new CustomError.DatabaseError();
+    });
+};
 
-const getAllEnrolledCourses = async(filter) => {
-  return await EnrolledCourses.find({userId: filter})
-    .then(data => data)
-    .catch(err => {
-      console.log(err)
-    })
-}
+const getCompletedEnrolledCourses = async (filter) => {
+  try {
+    const courses = await EnrolledCourses.find({ userId: filter, courseProgress: 100 })
+    let data = [];
+    for (const course of courses) {
+      const result = await courseDaos.findCourseById(course.courseId);
+      data.push(result);
+    }
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+  
+};
 
+const getInProgressEnrolledCourses = async (filter) => {
+  try {
+    const courses = await EnrolledCourses.find({ userId: filter, courseProgress: {$lt: 100} })
+    let data = [];
+    for (const course of courses) {
+      const result = await courseDaos.findCourseById(course.courseId);
+      data.push(result);
+    }
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+};
 export default {
   findEnrolledCourse,
   createNewEnrolledCourse,
   updateEnrolledCourse,
   deleteEnrolledCourse,
-  getAllEnrolledCourses
-}
+  getCompletedEnrolledCourses,
+  getInProgressEnrolledCourses
+};
