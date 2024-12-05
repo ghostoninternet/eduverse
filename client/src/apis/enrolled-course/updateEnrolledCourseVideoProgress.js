12a34701f7159f7b19f0360c616bc6c1ffa4ef85
phucpham <phamhuuphuc4049@gmail.com>
2024-12-05 23:35:02 +0700

    update course learning, completed watch video lesson + leave review

--- client/src/apis/enrolled-course/updateEnrolledCourseVideoProgress.js
+++ client/src/apis/enrolled-course/updateEnrolledCourseVideoProgress.js
@@ -1,14 +1,25 @@
 import customFetch from "../../helpers/customFetch";
 
-const updateEnrolledCourseVideoProgress = async (userId, courseId, updatedData) => {
-    const url = `http://localhost:8000/api/enrolled-courses/video-progress/${courseId}`;
-    try {
-      const response = await customFetch(url);
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const updateEnrolledCourseVideoProgress = async (
+  userId,
+  courseId,
+  updatedData
+) => {
+  const url = `http://localhost:8000/api/enrolled-courses/video-progress/${courseId}`;
+  try {
+    const response = await customFetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({moduleId: updatedData.moduleId, videoTitle: updatedData.videoTitle}),
+    });
+    const json = await response.json();
+    console.log(json)
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export default updateEnrolledCourseVideoProgress;
