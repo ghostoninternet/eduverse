a7ebbba3199912d12c9771896d76fb53fd0e7bab
ghostoninternet <daosyphuc288@gmail.com>
2024-12-09 16:43:44 +0700

    feature: Integrate course management api

---
+++ client/src/apis/review/getInstructorCourseReview.js
@@ -0,0 +1,14 @@
+import customFetch from "../../helpers/customFetch";
+
+const getInstructorCourseReview = async (courseId, limit, page) => {
+  const url = `http://localhost:8000/api/review/instructor/${courseId}?limit=${limit}&page=${page}`;
+  try {
+    const response = await customFetch(url);
+    const json = await response.json();
+    return json
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getInstructorCourseReview;
