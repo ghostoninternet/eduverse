22bcc61b9e9c6ff2fc684d49c6466c08b2ed37ab
phucpham <phamhuuphuc4049@gmail.com>
2024-12-16 21:22:34 +0700

    fix some small errors in updateExerciseProgressApi

--- server/src/controllers/enrolledCourseControllers.js
+++ server/src/controllers/enrolledCourseControllers.js
@@ -22,7 +22,7 @@ const updateEnrolledCourseVideoProgress = async (req, res, next) => {
 }
 
 const updateEnrolledCourseExerciseProgress = async (req, res, next) => {
-  const { userId } = req.userId
+  const { userId } = req
   const { courseId } = req.params
   const updatedEnrolledCourseProgress = await enrolledCourseService.updateEnrolledCourseExerciseProgress(userId, courseId, req.body)
   res.status(200).json(updatedEnrolledCourseProgress)
