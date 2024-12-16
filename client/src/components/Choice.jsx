0eaf0a45d154425ac9e49be5e6d43bfc8edcf0b3
phucpham <phamhuuphuc4049@gmail.com>
2024-12-17 00:02:59 +0700

    enhance UI for doing exercise in Course Learning

--- client/src/components/Choice.jsx
+++ client/src/components/Choice.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-const Choice = ({ label, content, handleSelectChoice, isSelected }) => {
+const Choice = ({
+  label,
+  content,
+  handleSelectChoice,
+  isSelected,
+  isAnswer,
+  isSubmitted,
+}) => {
   return (
-    <div className={`hover:bg-blue-300 rounded-md px-6 py-4 flex items-center text-xl border-2 cursor-pointer shadow-md ${isSelected && "bg-blue-300"}`} onClick={handleSelectChoice}>
+    <button
+      className={`${
+        !isSubmitted && "hover:bg-blue-300"
+      } rounded-md px-6 py-4 flex items-center text-xl border-2 shadow-md ${
+        isSelected && "bg-blue-300"
+      } ${(isSelected && isAnswer && isSubmitted) && "bg-green-500"} ${
+        (isSelected && !isAnswer && isSubmitted) && "bg-red-500"
+      }`}
+      disabled={isSubmitted}
+      onClick={handleSelectChoice}
+    >
       {label}. {content}
-    </div>
+    </button>
+    
   );
 };
 Choice.propTypes = {
   label: PropTypes.string,
   content: PropTypes.string,
+  handleSelectChoice: PropTypes.func,
+  isSelected: PropTypes.bool,
+  isAnswer: PropTypes.bool,
+  isSubmitted: PropTypes.bool,
 };
 export default Choice;
--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 const Video = ({title, index,isChecked, onClick, videoLength, isPlaying}) => {
   return (
     <div className={`flex p-4 gap-x-4 items-center cursor-pointer hover:bg-gray-300 ${isPlaying && "bg-gray-300 "}`} onClick={onClick}> 
-      <div className=''>
+      <div className='w-1/12 flex items-center justify-center'>
         <input type='checkbox' style={{ width: '20px', height: '20px' }} className='cursor-pointer' disabled checked={isChecked} />
       </div>
-      <div className='flex flex-col gap-y-2'>
+      <div className='w-11/12 flex flex-col gap-y-2'>
         <p className='text-xl'>{index}. {title}</p>
         <div className='flex gap-x-2 text-gray-500'>
             <OndemandVideoIcon/>
