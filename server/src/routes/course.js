697745380b2371b9264090b78571a390d6e8fdde
quanpva62 <phamquan6023@gmail.com>
2024-12-21 15:55:16 +0700

    restore it back to the original

--- server/src/routes/course.js
+++ server/src/routes/course.js
@@ -20,18 +20,7 @@ courseRouter.get('/instructor', asyncHandler(courseControllers.getInstructorCour
 courseRouter.get('/instructor/all-courses', asyncHandler(courseControllers.getAllInstructorCourseTitles))
 courseRouter.get('/instructor/search', asyncHandler(courseControllers.searchInstructorCourses))
 courseRouter.post('/instructor', asyncHandler(courseControllers.createNewCourse))
-courseRouter.get('/instructor/:courseId', async (req, res, next) => {
-    const { courseId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(courseId)) {
-      // Xử lý trường hợp courseSlug được truyền
-      const course = await courseDaos.findCourseBySlug({ courseSlug: courseId });
-      if (!course) {
-        return res.status(400).json({ error: "Invalid course ID or slug" });
-      }
-      req.params.courseId = course._id; // Chuyển đổi slug thành ObjectId
-    }
-    next();
-  }, asyncHandler(courseControllers.getInstructorCourseDetail));
+courseRouter.get('/instructor/:courseId', asyncHandler(courseControllers.getInstructorCourseDetail))
 courseRouter.put('/instructor/:courseId', asyncHandler(courseControllers.updateCourse))
 courseRouter.delete('/instructor/:courseId', asyncHandler(courseControllers.deleteCourse))
 
