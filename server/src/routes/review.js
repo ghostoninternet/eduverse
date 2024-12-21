34edd5b112a83238e2365e7f2460ba6c7e7b38cf
quanpva62 <phamquan6023@gmail.com>
2024-12-21 15:55:16 +0700

    restore it back to the original

--- server/src/routes/review.js
+++ server/src/routes/review.js
@@ -2,24 +2,12 @@ import express from 'express'
 import asyncHandler from '../middlewares/asyncHandler.js'
 import authToken from '../middlewares/authToken.js'
 import reviewControllers from '../controllers/reviewControllers.js'
-import courseDaos from '../daos/courseDaos.js'
-import mongoose from 'mongoose';
+
 
 const reviewRouter = express.Router()
 
 reviewRouter.use(authToken)
-reviewRouter.get('/:courseId', async (req, res, next) => {
-    const { courseId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(courseId)) {
-      // Tìm kiếm courseId từ courseSlug
-      const course = await courseDaos.findCourseBySlug({ courseSlug: courseId });
-      if (!course) {
-        return res.status(400).json({ error: "Invalid course ID or slug" });
-      }
-      req.params.courseId = course._id; // Cập nhật params
-    }
-    next();
-  }, asyncHandler(reviewControllers.getCourseReviews));
+reviewRouter.get('/:courseId', asyncHandler(reviewControllers.getCourseReviews))
 reviewRouter.post('/', asyncHandler(reviewControllers.createCourseReview))
 reviewRouter.put('/:reviewId', asyncHandler(reviewControllers.updateCourseReview))
 reviewRouter.delete('/:reviewId', asyncHandler(reviewControllers.deleteCourseReview))
