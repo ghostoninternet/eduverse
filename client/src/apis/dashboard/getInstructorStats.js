c3d1f705549226a479dfcf284f7315b0d1a8fc1e
quanpva62 <phamquan6023@gmail.com>
2024-12-23 10:25:35 +0700

    instructor dashboard

---
+++ client/src/apis/dashboard/getInstructorStats.js
@@ -0,0 +1,14 @@
+import customFetch from "../../helpers/customFetch";
+
+export default async function getInstructorStats() {
+  try {
+    const response = await customFetch("http://localhost:8000/api/dashboard/instructor");
+    const data = await response.json();
+    console.log("API response:", data); // Log the entire response
+    return data;
+  } catch (error) {
+    console.error("Error fetching instructor stats:", error);
+    return { data: null };
+  }
+}
+
